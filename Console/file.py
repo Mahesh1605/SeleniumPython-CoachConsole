2248dcfc7dd1bec6b1a707fd17ffbf6635d0c1f9
Mahesh1605 <zenqtestuser2beyond12@gmail.com>
2023-01-05 03:19:24 +0530

    update

--- Console/file.py
+++ Console/file.py
@@ -1,6 +1,8 @@
 import time
+from datetime import date
 
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
@@ -15,21 +17,9 @@ driver.maximize_window()
 driver.find_element(By.XPATH, "//input[@ng-model='ctrl.email']").send_keys("ananthamahesh.yeruva@zenq.com")
 driver.find_element(By.XPATH, "//input[@ng-model='ctrl.password']").send_keys("Letmein@123")
 driver.find_element(By.XPATH, "//button[@type='submit']").click()
-time.sleep(10)  # Sleep command to enter 2FA code
+time.sleep(15)  # Sleep command to enter 2FA code
 driver.find_element(By.XPATH, "//span[@class='ng-scope']").click()
 time.sleep(5)
 print(driver.title)
 
-# Create an Event with advanced recipient and plane text
-driver.find_element(By.XPATH, "//a[@aria-label='Events']").click()
-driver.find_element(By.XPATH, "//a[@ng-href='/events/add']").click()
-time.sleep(3)
-driver.find_element(By.XPATH, "//md-select[@name='district']").click()
-District = driver.find_elements(By.XPATH, "//div[@class='md-select-menu-container md-active md-clickable']")
-for Select_districts in District:
-
-    if Select_districts.text == "Apple Valley Test":
-        Select_districts.click()
-        print(Select_districts)
-    break
 
